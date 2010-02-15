97ef3f6aa5ccc7e2df4a84491416c16b49102d03
Euzel Villanueva <dumas045@gmail.com>
2010-02-15 04:20:46 +0000

    Fixed service and got server to respond to client. (Still extremely alpha...)

--- ODKClinicServer/src/com/odkclinic/db/hibernate/HibernateRevisionTokenDAO.java
+++ ODKClinicServer/src/com/odkclinic/db/hibernate/HibernateRevisionTokenDAO.java
@@ -1,6 +1,6 @@
+
 package com.odkclinic.db.hibernate;
 
-import java.sql.SQLException;
 import java.util.Date;
 
 import org.apache.commons.logging.Log;
@@ -9,45 +9,55 @@ import org.hibernate.SessionFactory;
 
 import com.odkclinic.db.RevisionTokenDAO;
 
-public class HibernateRevisionTokenDAO implements RevisionTokenDAO {
-	protected final Log log = LogFactory.getLog(getClass());
-	
-	private SessionFactory sessionFactory;
-	
-	
-	public void setSessionFactory(SessionFactory sessionFactory)
+public class HibernateRevisionTokenDAO implements RevisionTokenDAO
+{
+    protected final Log log = LogFactory.getLog(getClass());
+
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory)
     {
         this.sessionFactory = sessionFactory;
     }
-	
-	@Override
-	public Date getRevisionToken(String table, int id) {
-		String sql = "select revision_token from odkclinic_" + table + " where id = "+ id;
-		log.debug("querying for revision token");
-		Date date = null;
-		try {date = (Date)sessionFactory.getCurrentSession().createSQLQuery(sql).uniqueResult();}
-		catch(Exception e) {
-			log.error("query for rev token fail", e);
-		}
-		
-		return date;
-	}
+
+    @Override
+    public Date getRevisionToken(String table, int id)
+    {
+        String sql = "select revision_token from odkclinic_" + table
+                + " where id = " + id;
+        log.debug("querying for revision token");
+        Date date = null;
+        try
+        {
+            date = (Date) sessionFactory.getCurrentSession()
+                    .createSQLQuery(sql).uniqueResult();
+        } catch (Exception e)
+        {
+            log.error("query for rev token fail", e);
+        }
+
+        return date;
+    }
 
     @Override
     public long getLargestRevisionToken(String table)
     {
-        String sql = "select revision_token from odkclinic_" + table + " order by revision_token DESC LIMIT 1";
+        String sql = "select revision_token from odkclinic_" + table
+                + " order by revision_token DESC LIMIT 1";
         log.debug("querying for revision token");
         Date date = null;
-        try {date = (Date)sessionFactory.getCurrentSession().createSQLQuery(sql).uniqueResult();}
-        catch(Exception e) {
+        try
+        {
+            date = (Date) sessionFactory.getCurrentSession()
+                    .createSQLQuery(sql).uniqueResult();
+        } catch (Exception e)
+        {
             log.error("query for rev token fail", e);
         }
         if (date != null)
             return date.getTime();
-        else 
+        else
             return -1;
     }
-	
-	
+
 }
